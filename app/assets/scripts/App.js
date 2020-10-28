0985125928568d9de0c3973c0beeca534c1a9aed
Amber <ambergjones14@yahoo.com>
2020-10-28 23:08:01 +0200

    Completed build process configuration.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,6 @@ import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js'
 import StickyHeader from './modules/StickyHeader.js'
 
-
 new StickyHeader ()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
