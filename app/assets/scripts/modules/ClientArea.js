76f4363447a37c7972111b38e1690519f17752ca
Amber <ambergjones14@yahoo.com>
2020-10-30 16:57:22 +0200

    Added and corrected client feature.

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -21,7 +21,7 @@ class ClientArea {
             this.form.remove()
             this.contentArea.innerHTML = response.data 
         }).catch(() => {
-            this.contentArea.innerHTML =`<p> class= "client-area__error">That secret phrase is not correct, please try again</p>`
+            this.contentArea.innerHTML =`<p class= "client-area__error">That secret phrase is not correct. Please try again. </p>`
             this.field.value = ''
             this.field.focus()
         })
